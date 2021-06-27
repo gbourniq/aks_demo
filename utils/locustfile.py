de33d6d6d90ce84dbd8405a7666989ebd4750ddf
Guillaume Bournique <gbournique@gmail.com>
2021-06-27 09:48:59 +0200

    :white_check_mark: add locust package and include load testing in cd pipeline

---
+++ utils/locustfile.py
@@ -0,0 +1,24 @@
+"""
+This file defines load testing behaviours for the web application
+"""
+
+
+from locust import HttpUser, between, task
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 1.5)
+
+    @task(10)
+    def index(self):
+        self.client.get("/")
+
+    # @task(3)
+    # def view_items(self):
+    #     for item_id in range(10):
+    #         self.client.get(f"/item?id={item_id}", name="/item")
+    #         time.sleep(1)
+
+    def on_start(self):
+        pass
+        # self.client.post("/login", json={"username":"foo", "password":"bar"})
